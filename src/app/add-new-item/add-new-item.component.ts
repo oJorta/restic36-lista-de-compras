68b1824ff0c983f0cb9fd4f49754fe19c627662f
Joao Pedro <jpcsnoberto@gmail.com>
2024-10-02 23:51:15 -0300

    feat: base structure, header and add-new-item input

---
+++ src/app/add-new-item/add-new-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-new-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-new-item.component.html',
+  styleUrl: './add-new-item.component.css'
+})
+export class AddNewItemComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddNewItemComponent } from "./add-new-item/add-new-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddNewItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
