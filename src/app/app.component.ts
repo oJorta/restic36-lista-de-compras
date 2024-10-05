0da392f8e534f990d0f75ab3ed9e84a984a90909
Joao Pedro <jpcsnoberto@gmail.com>
2024-10-05 02:34:57 -0300

    feat: added localStorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddNewItemComponent } from "./add-new-item/add-new-item.component";
 import { ShoppingListItemComponent } from "./shopping-list-item/shopping-list-item.component";
@@ -13,6 +13,14 @@ import { HeaderComponent } from "./header/header.component";
 })
 export class AppComponent {
   items = Array<{ itemName: string, isBought: boolean }>();
+
+  ngOnInit() {
+    const items = localStorage.getItem('items');
+
+    if (items) {
+      this.items = JSON.parse(items);
+    }
+  }
   
   addNewItem(itemName: string) {
     this.items.push({
@@ -20,23 +28,31 @@ export class AppComponent {
       isBought: false,
     });
     this.sortItems();
+    this.saveItems();
   }
-
+  
   handleUpdateName(id: number, itemName: string) {
     this.items[id].itemName = itemName;
+    this.saveItems();
   }
-
+  
   handleToggleBought(id: number, isBought: boolean) {
     this.items[id].isBought = isBought;
     this.sortItems();
+    this.saveItems();
   }
   
   handleDeleteItem(id: number) {
     this.items.splice(id, 1);
     this.sortItems();
+    this.saveItems();
   }
-
+  
   sortItems() {
     this.items.sort((a, b) => a.isBought === b.isBought ? 0 : a.isBought ? 1 : -1);
+  }
+
+  saveItems() {
+    localStorage.setItem('items', JSON.stringify(this.items));
   }
 }
