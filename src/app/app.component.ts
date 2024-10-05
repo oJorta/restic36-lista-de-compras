345ff2d9c6373a389015b87ff832bc0cac21fb95
Joao Pedro <jpcsnoberto@gmail.com>
2024-10-05 01:03:17 -0300

    fix: created a header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AddNewItemComponent } from "./add-new-item/add-new-item.component";
 import { ShoppingListItemComponent } from "./shopping-list-item/shopping-list-item.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AddNewItemComponent, ShoppingListItemComponent],
+  imports: [RouterOutlet, AddNewItemComponent, ShoppingListItemComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
